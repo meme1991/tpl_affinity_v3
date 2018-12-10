b07be6a39b6e3ea857a459f822250d8734976868
Emanuele Cappato <e.kappato@icloud.com>
2018-12-10 09:57:52 +0100

    Add files via upload

--- offline.php
+++ offline.php
@@ -1,16 +1,8 @@
 <?php defined( '_JEXEC' ) or die; ?>
 <?php
-
 	// variables
 	$app            = JFactory::getApplication();
-	// $doc            = JFactory::getDocument();
-	// $menu           = $app->getMenu();
-	// $active         = $app->getMenu()->getActive();
-	// $params         = $app->getParams();
-	// $pageclass      = $params->get('pageclass_sfx');
 	$tpath          = $this->baseurl.'/templates/'.$this->template;
-	// $templateparams	= $app->getTemplate(true)->params;
-
 	// template params
 	$siteName      = $app->get('sitename');
  ?>
@@ -19,28 +11,19 @@
 <head>
 	<title><?php echo $siteName ?> - <?php echo JText::_("TPL_AFFINITY_OFFLINE_HEADER") ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="<?php echo $tpath; ?>/dist/font5/css/fontawesome-all.min.css" rel="stylesheet" type="text/css" />
-	<link href="<?php echo $tpath; ?>/css/offline.min.css?ver=1.0.0" rel="stylesheet" type="text/css" />
+	<link href="<?php echo $tpath; ?>/css/offline.min.css?ver=2.0.0" rel="stylesheet" type="text/css" />
+	<meta name="robots" content="noindex, nofollow">
 </head>
 <body>
-
-	<div class="container" style="margin-top: 200px;">
-		<div class="row">
-			<div class="col-12" style="text-align:center">
-				<i class="fal fa-cogs fa-3x"></i>
-				<h1 class="display-4"><?php echo JText::_("TPL_AFFINITY_OFFLINE_HEADER") ?></h1>
-				<?php if ($app->getCfg('display_offline_message', 1) == 1 && str_replace(' ', '', $app->getCfg('offline_message')) != ''): ?>
-				<p class="my-3 lead">
-					<?php echo $app->getCfg('offline_message') ?>
-				</p>
-				<?php else: ?>
-				<p class="my-3 lead">
-					<?php echo JText::_("TPL_AFFINITY_OFFLINE_DEFAULT_MESSAGE") ?>
-				</p>
-				<?php endif; ?>
-			</div>
+	<div class="main">
+		<div class="display-error">
+			<span class="display-4 d-block"><?php echo JText::_("TPL_AFFINITY_OFFLINE_HEADER") ?></span>
+			<?php if ($app->getCfg('display_offline_message', 1) == 1 && str_replace(' ', '', $app->getCfg('offline_message')) != ''): ?>
+				<span class="message d-block"><?php echo $app->getCfg('offline_message') ?></span>
+			<?php else: ?>
+				<span class="message d-block"><?php echo JText::_("TPL_AFFINITY_OFFLINE_DEFAULT_MESSAGE") ?></span>
+			<?php endif; ?>
 		</div>
 	</div>
-
 </body>
 </html>
