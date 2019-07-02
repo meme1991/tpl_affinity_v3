ab5ea4c4372f5853b927ede711334b03dc237a2d
Emanuele Cappato <e.kappato@icloud.com>
2019-07-02 09:55:38 +0200

    Update default.php

--- html/mod_custom/default.php
+++ html/mod_custom/default.php
@@ -9,6 +9,7 @@
 
 defined('_JEXEC') or die;
 ?>
-<div class="custom<?php echo $moduleclass_sfx; ?>" <?php if ($params->get('backgroundimage')) : ?> style="background-image:url(<?php echo $params->get('backgroundimage'); ?>)"<?php endif; ?> >
+<?php $col = ($params->get('bootstrap_size')) ? "col-12 col-lg-".$params->get('bootstrap_size') : ""; ?>
+<div class="custom<?php echo $moduleclass_sfx; ?> <?= $col ?>" <?php if ($params->get('backgroundimage')) : ?> style="background-image:url(<?php echo $params->get('backgroundimage'); ?>)"<?php endif; ?> >
 	<?php echo $module->content; ?>
 </div>
